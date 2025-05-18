632b62a282a3f4a433578a0e41d46c7ca062f42b
Lucasfi <lukafigueroa64@gmail.com>
2025-05-17 21:59:57 -0300

    modificamos logica para agregar tarea, agregando form y usando evento submit

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 
-const agregarTarea = () => {
-
-    let nuevaTareaTexto = document.getElementById("nuevaTarea").value;
-    if(nuevaTareaTexto === "") {
-        alert('Por favor ingrese una tarea');
-        return;
-    }
-
-    let nuevaTarea = document.createElement('li');
-    nuevaTarea.textContent = nuevaTareaTexto + "";
-
-    document.getElementById('listaTareas').appendChild(nuevaTarea);
-    document.getElementById('nuevaTarea').value = "";
+const agregarTarea = (e) => {
+    e.preventDefault();
+    console.log('Aqui deberia agregar una tarea'); 
+    //necesito tomar el texto del input
+    const inputForm = document.getElementById('nuevaTarea').value;
+    //necesito el ul padre para agregar un nuevo item hijo li
+    const lista = document.querySelector('.list-group')
+    //necesito agregar un li con el texto del input 
+    lista.innerHTML += `<li class="list-group-item">${inputForm}</li>`
+    //limpiar el formulario
+    formularioTarea.reset();
 }
+
+const formularioTarea = document.getElementById('tareaForm');
+formularioTarea.addEventListener('submit', agregarTarea)
